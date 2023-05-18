acf6bb3bf7c759ef8c4be0356d1fe4f2c94783c5
Tenhag10 <129264295+Tenhag10@users.noreply.github.com>
2023-05-18 22:33:59 +0700

    Add files via upload

---
+++ lib/models/user.dart
@@ -0,0 +1,50 @@
+class User {
+  final int id;
+  final String name;
+  final String userName;
+  final String email;
+  final String? profileImage;
+  final String? phoneNumber;
+
+  User({
+    required this.id,
+    required this.name,
+    required this.userName,
+    required this.email,
+    this.profileImage,
+    this.phoneNumber,
+  });
+
+  factory User.fromMap(Map<String, dynamic> data) {
+    return User(
+      id: data["id"],
+      name: data["name"],
+      userName: data["user_name"],
+      email: data["email"],
+      profileImage: data["profile_image"],
+      phoneNumber: data["phone_number"],
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      "id": id,
+      "name": name,
+      "user_name": userName,
+      "email": email,
+      "profile_image": profileImage,
+      "phoneNumber": phoneNumber,
+    };
+  }
+
+  factory User.dummy() {
+    return User(
+      id: 01,
+      name: "Farhan Nurdiansyah",
+      userName: "TenHag10",
+      email: "2006119@itg.ac.id",
+      profileImage: "botak.jpg",
+      phoneNumber: "088997801233",
+    );
+  }
+}
